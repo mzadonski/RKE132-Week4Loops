1acb995c1e25a01b83af6ed7317e912c6ab36e9c
MZadonski <maria.zadons@tktk.ee>
2023-05-02 00:09:53 +0300

    add week4loops

---
+++ Week4Loops/Program.cs
@@ -0,0 +1,13 @@
+﻿// FOR-LOOP
+//DRY-priciple - Do not repeat yourself
+
+int i = 0;
+
+for(i = 0; i < 5; i++) //i++ = i + i
+{
+    Console.WriteLine("We dont need no education.");
+}
+
+Console.WriteLine(i);
+Console.WriteLine("Have a nice day.");
+
---
+++ WhileLoop/Program.cs
@@ -0,0 +1,24 @@
+﻿
+Random rnd = new Random();
+int cpuRandom;
+    bool LoopActive = true; //bool - true/fals
+while (LoopActive)
+{
+    cpuRandom = rnd.Next(1, 4);
+    //Console.WriteLine($"cpu has generated {cpuRandom}.");
+    Console.WriteLine("Make a guess. Enter a number 1-3");
+
+    int userName = Int32.Parse(Console.ReadLine());
+
+    if (userName == cpuRandom)
+    {
+        Console.WriteLine("Congratulation, you won!");
+        break;
+    }
+    else
+    {
+       Console.WriteLine("Oops. Try again.");
+    }
+}
+
+Console.WriteLine("Have a nice day!");
